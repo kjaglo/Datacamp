bd0c35bcfa472051ddd3533e85d217e1e8943364
kinga j <kingajaglo.99@gmail.com>
2021-04-06 17:55:30 +0200

    feat: read csv

--- read_csv.py
+++ read_csv.py
@@ -1,1 +1,20 @@
-print("hello")
+import pandas as pd
+
+data = pd.read_csv("data.csv", sep=";")
+
+print(data)
+
+print(data.head())
+
+print(data.info())
+
+ages = data["age"]
+
+print(ages)
+
+print(ages[0])
+
+olderThan30 = data[data.age > 30]
+
+print(olderThan30)
+
