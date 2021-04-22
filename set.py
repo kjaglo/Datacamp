e745b779d7be7e1587acb83f2c674f120b1993e1
kinga j <kingajaglo.99@gmail.com>
2021-04-22 23:59:27 +0200

    feat: introducing DataFrames

---
+++ set.py
@@ -0,0 +1,18 @@
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+# no double values: {'apple', 'cherry', 'banana'}
+
+print(thisset)
+
+print(len(thisset))
+
+set1 = {"abc", 34, True, 40, "male"}
+
+print(type(thisset))
+
+thisset = {"apple", "banana", "cherry","apple", "banana", "cherry"}
+
+# {'banana', 'cherry', 'apple'}
+
+print(thisset)
