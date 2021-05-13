51a625dfa0772166aba0bf5c21c3a4ba85b5f219
kinga j <kingajaglo.99@gmail.com>
2021-05-13 23:52:32 +0200

    add: stack

---
+++ data/queue.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+queue = deque([3, 4, 5])  # deque([3, 4, 5])
+print(queue)  # deque([3, 4, 5])
+queue.append(6)  # deque([3, 4, 5, 6])
+print(queue)  # deque([3, 4, 5, 6])
+print(queue.popleft())  # 3
+print(queue)  # deque([4, 5, 6])
+
+#create: queue
--- stack.py
+++ stack.py
@@ -2,8 +2,8 @@ stack = [1, 2, 3]
 stack.append(4)
 stack.append(5)
 stack.append(6)
-print(stack)
-print(stack.pop())
-print(stack)
-print(stack.pop())
-print(stack)
+print(stack)  # [1, 2, 3, 4, 5, 6]
+print(stack.pop())  # 6
+print(stack)  # [1, 2, 3, 4, 5]
+print(stack.pop())  # 5
+print(stack)  # [1, 2, 3, 4]
