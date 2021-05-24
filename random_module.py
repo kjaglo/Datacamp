1242eaad43a63d1fe8d7e7e446528468d2b80627
kinga j <kingajaglo.99@gmail.com>
2021-05-25 00:00:37 +0200

    create: random

---
+++ random_module.py
@@ -0,0 +1,50 @@
+import random
+
+dir(random)
+
+print(help(random.random))  # random() -> x in the interval [0, 1).
+
+for i in range(10):
+    print(i)  # 0,1,2...9
+    print(random.random())  # [0,1)
+
+for i in range(10):
+    random_number_3_7 = random.random() * 4 + 3
+    print(random_number_3_7)  # [3,7)
+
+print(help(random.uniform))
+#  Get a random number in the range [a, b) or [a, b] depending on rounding.
+for i in range(10):
+    print(random.uniform(3, 7))  # [3,7)
+
+print(help(random.normalvariate))
+
+# normalvariate(mu, sigma) method of random.Random instance
+#   Normal distribution.
+#   mu is the mean, and sigma is the standard deviation.
+
+a = []
+for i in range(10000):
+    a.append(random.normalvariate(mu=0, sigma=1))
+from matplotlib import pyplot as plt
+
+plt.hist(a)
+plt.show()
+
+for i in range(10000):
+    a.append(random.normalvariate(mu=0, sigma=9))
+from matplotlib import pyplot as plt
+
+plt.hist(a)
+plt.show()
+
+print(help(random.randint))
+
+# randint(a, b) method of random.Random instance
+# Return random integer in range [a, b], including both end points.
+
+for i in range(10):
+    print(random.randint(1, 10))
+
+
+game = ['paper', 'rock']
